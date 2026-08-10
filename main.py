3cb92ce921883c6b5294c0966d8736ae82643ef8
zoherr <workforzoher@gmail.com>
2026-08-10 23:07:27 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"health": "OK"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
